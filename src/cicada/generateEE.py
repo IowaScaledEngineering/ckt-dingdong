d41ca45d6506bfc716e1f649300f5c48e53504a0
Michael Petersen <railfan@drgw.net>
2020-08-20 20:26:03 -0600

    Random EE generator

---
+++ src/cicada/generateEE.py
@@ -0,0 +1,12 @@
+from intelhex import IntelHex
+import random
+
+ih = IntelHex()
+
+for i in range(0,256):
+#  ih[2*i] = i
+#  ih[2*i+1] = 10
+  ih[2*i] = random.randint(0,255)
+  ih[2*i+1] = random.randint(0,255)
+
+ih.write_hex_file("cicada.eep")
