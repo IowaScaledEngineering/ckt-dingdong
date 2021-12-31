2eb505a2a38c0c1f9cc02d0116de4574f8514d4f
Michael Petersen <railfan@drgw.net>
2021-12-30 20:05:41 -0700

    EE generator for demo boards (short delay)

---
+++ src/cicada/generateEE-demo.py
@@ -0,0 +1,15 @@
+from intelhex import IntelHex
+import random
+
+ih = IntelHex()
+
+for i in range(0,256):
+#  ih[2*i] = i
+#  ih[2*i+1] = 10
+  ih[2*i] = random.randint(0,255)
+  ih[2*i+1] = 5
+
+  # Make initial delay 1 sec on all units
+#  ih[1] = 10
+
+ih.write_hex_file("cicada.eep")
